97f844f02d7eb8d93b5fbcb186e821cc06a32a05
Sylar48 <andrewkuznecov48@yandex.ru>
2023-04-18 15:36:28 +0300

    readme added

--- pages/locators.py
+++ pages/locators.py
@@ -4,8 +4,6 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-
-
 class LoginPageLocators():
     login_url = "http://selenium1py.pythonanywhere.com/"
 
